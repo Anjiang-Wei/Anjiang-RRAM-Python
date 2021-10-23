21255ebaecc5815a5ea6e9547be5b0ff992aaf37
Anjiang-Wei <1020681930@qq.com>
2021-10-23 11:19:51 -0700

    dead detection for 10 cells complete

--- dead_detection.py
+++ dead_detection.py
@@ -6,6 +6,7 @@ chip_name = "C10"
 def detect(cells, low=10*1e3, high=11*1e3):
     for addr in cells:
         assert test_scheme.start_addr <= addr and addr < test_scheme.end_addr
+        nisys.set_addr(addr)
         target = nisys.target(low, high)
         if low <= target[0] and target[0] <= high:
             continue
@@ -21,5 +22,6 @@ def detect_after_test(ncells):
 if __name__ == "__main__":
     fout = open("log/new_dead.csv", "a")
     nisys = NIRRAM(chip_name)
+    detect_after_test(10)
     nisys.close()
     fout.close()
