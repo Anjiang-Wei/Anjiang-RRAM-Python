538d07e6fc682eda713dc5b775250fa6ee66a7f2
Anjiang-Wei <1020681930@qq.com>
2021-10-25 21:53:04 +0000

    Add TinyLevel util class

---
+++ utils/TinyLevel.py
@@ -0,0 +1,53 @@
+class Tiny_Level(object):
+    all_levels = []
+    def __init__(self, low, high, success):
+        assert low < high
+        self.low = low
+        self.high = high
+        self.width = high - low
+        self.center = (low + high) / 2
+        self.success = success
+        self.summary = {0: 0, -1: 0, 1: 0}
+        self.summary[success] = 1
+    
+    def __eq__(self, o):
+        return self.low == o.low and self.high == o.high
+    
+    def __str__(self):
+        success_total = self.summary[0]
+        lower_total = self.summary[-1]
+        higher_total = self.summary[1]
+        total_times = sum(self.summary.values())
+        assert success_total + lower_total + higher_total == total_times
+        return f"Width:{self.width}, Center:{self.center}, \
+            Success:{success_total}/{total_times} = {success_total/total_times}, \
+            Low:{lower_total}, High:{higher_total}"
+    
+    @staticmethod
+    def add(o):
+        '''
+        Update the single result to all levels
+        Initialize a new level when needed
+        '''
+        if o not in Tiny_Level.all_levels:
+            Tiny_Level.all_levels.append(o)
+        else:
+            idx = Tiny_Level.all_levels.index(o)
+            Tiny_Level.all_levels[idx].summary[o.success] += 1
+
+    @staticmethod
+    def low2idx(low, levels=all_levels):
+        '''
+        Given a low value, return the index (redundance removed) to it w.r.t all the levels specified
+        '''
+        all_lows = list(map(lambda x: x.low), levels)
+        sorted_lows = sorted(list(set(all_lows)))
+        return sorted_lows.index(low)
+    
+    @staticmethod
+    def filter_levels(lambda_func):
+        '''
+        Given a lambda function, return the filtered levels from all_levels
+        '''
+        satisfied_levels = list(filter(lambda_func, Tiny_Level.all_levels))
+        return satisfied_levels
