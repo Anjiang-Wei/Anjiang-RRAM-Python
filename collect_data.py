587ade2ca02e55986854e86f46aa5bc7df95bac3
Anjiang-Wei <1020681930@qq.com>
2021-10-26 03:44:11 +0000

    new exp script done

--- collect_data.py
+++ collect_data.py
@@ -13,7 +13,8 @@ high_init_config = {
     "B": [86666.666, 200 * 1e3]
 }
 
-random_seed = 4
+random_seed = 5
+exp_id = 0
 
 def write_init(addr):
     nisys.set_addr(addr)
@@ -26,6 +27,9 @@ def write_init(addr):
 
 def write(addr, target_low_res, target_hi_res, attempts):
     assert addr >= start_addr and addr < end_addr
+    if target_low_res < 7812.5:
+        print("Too low, skip!")
+        return
     nisys.set_addr(addr)
     target = nisys.target(target_low_res, target_hi_res, max_attempts=attempts)
     log.write(f"Write\t{target_low_res}\t{target_hi_res}\t{addr}\t{time.time()}\t{target[0]}\t{target[1]}\t{attempts}\n")
@@ -68,13 +72,17 @@ def random_pick(ncells):
 def collect(ncells):
     dead_log = open("log/new_dead.csv", "a")
     cells = random_pick(ncells)
-    w_center = 12000
-    for addr in cells:
-        print(addr)
-        for width in [50, 100, 200, 500]:
+    w_centers = [7822.5, 8002.5, 8182.5, 8362.5, 8542.5, \
+                8722.5, 8902.5, 9082.5, 9382.5, 9792.5, \
+                10202.5, 10612.5, 11342.5, 12452.5, 14382.5, \
+                18682.5, 22000.0, 26442.5, 32000.0, 39502.5]
+    for w_center in w_centers:
+        for width in range(50, 1000, 100):
             for num_attempts in [10, 25, 50, 100]:
-                write_init(addr)
-                write(addr, w_center-width/2, w_center+width/2, num_attempts)
+                for addr in cells:
+                    print(addr)
+                    write_init(addr)
+                    write(addr, w_center-width/2, w_center+width/2, num_attempts)
     print("Start dead cell detection")
     dead_detection.detect(cells, dead_log, nisys)
     dead_log.close()
@@ -85,7 +93,7 @@ if __name__ == "__main__":
     print("Num of dead cells", len(dead_cells))
     nisys = NIRRAM(chipname)
     n_cells = 10
-    log = open(f"testlog/collect_data_{n_cells}_{random_seed}_0", "w")
+    log = open(f"testlog/collect_data_{n_cells}_{random_seed}_{exp_id}", "w")
     collect(n_cells)
     nisys.close()
     log.close()
