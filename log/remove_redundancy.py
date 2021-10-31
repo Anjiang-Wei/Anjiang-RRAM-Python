7ff2320769e3197b5f6188c96ae49d4d7435fae6
Anjiang-Wei <1020681930@qq.com>
2021-10-31 18:45:29 +0000

    remove redundancy

---
+++ log/remove_redundancy.py
@@ -0,0 +1,12 @@
+deads = []
+to_write = []
+with open("new_dead.csv", "r") as fin:
+    lines = fin.readlines()
+    for line in lines:
+        addr = line.strip().split(",")[0]
+        if addr not in deads:
+            deads.append(addr)
+            to_write.append(line)
+
+with open("new_dead2.csv", "w") as fout:
+    fout.writelines(to_write)
