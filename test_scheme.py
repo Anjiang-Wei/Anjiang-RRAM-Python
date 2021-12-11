bb33abfe8df9e977b0f68d2076c4a488a0c27e92
Anjiang-Wei <1020681930@qq.com>
2021-12-11 03:24:11 +0000

    Everything seems ready to launch hardware testing for SBA

--- test_scheme.py
+++ test_scheme.py
@@ -6,25 +6,27 @@ import dead_detection
 
 chipname = "C13"
 config_char = "B"
-exp_id = 6
+exp_id = 4
 start_addr = 0
 end_addr = 65536
 dead_cells = []
 test_scheme_files = [
-    'scheme/C13_6.json',
-    'scheme/C13_7.json',
-    'scheme/C13_8.json',
-    'scheme/C13_9.json',
-    'scheme/C13_10.json',
-    'scheme/C13_11.json',
-    'scheme/C13_12.json',
-    'scheme/C13_13.json',
-    'scheme/C13_14.json',
-    'scheme/C13_15.json',
-    'scheme/C13_16.json'
+    'scheme/SBA/C13_both_4.json',
+    'scheme/SBA/C13_both_5.json',
+    'scheme/SBA/C13_both_6.json',
+    'scheme/SBA/C13_both_7.json',
+    'scheme/SBA/C13_both_8.json',
+    'scheme/SBA/C13_both_9.json',
+    'scheme/SBA/C13_both_10.json',
+    'scheme/SBA/C13_both_11.json',
+    'scheme/SBA/C13_both_12.json',
+    'scheme/SBA/C13_both_13.json',
+    'scheme/SBA/C13_both_14.json',
+    'scheme/SBA/C13_both_15.json',
+    'scheme/SBA/C13_both_16.json',
 ]
-random_seed = 3 + exp_id
-levels = Level.load_from_file(test_scheme_files[exp_id-6])
+random_seed = 16 + exp_id
+levels = Level.load_from_file(test_scheme_files[exp_id-4])
 high_init_config = {
     "B": [levels[-1].r1, levels[-1].r2]
 }
@@ -102,7 +104,7 @@ if __name__ == "__main__":
     print("Num of dead cells", len(dead_cells))
     nisys = NIRRAM(chipname)
     n_cells = 100
-    log = open(f"testlog/13scheme_test_{n_cells}_{random_seed}_{exp_id}_Dec6", "w")
+    log = open(f"testlog/13scheme_test_{n_cells}_{random_seed}_{exp_id}_Dec10", "w")
     testscheme(n_cells)
     nisys.close()
     log.close()
