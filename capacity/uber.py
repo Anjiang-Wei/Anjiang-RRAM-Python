646d94762732a260bb15cf01d92bd0654f2e6233
Anjiang-Wei <1020681930@qq.com>
2022-01-04 04:22:10 +0000

    bug fixed

--- capacity/uber.py
+++ capacity/uber.py
@@ -1,5 +1,6 @@
 from scipy.special import comb
 import pickle
+import math
 
 db = {}
 # (q, n, k) --> d
@@ -18,33 +19,23 @@ def P_cw(N, E, RBER):
         res += comb(N, i) * pow(RBER, i) * pow(1-RBER, N-i)
     return res
 
-def BCH():
-    '''
-    For any positive integer m >= 3 and t < 2^m-1, there exists a binary BCH code:
-    Block length: n = 2^m - 1
-    Number of parity-check digits: n - k ≤ mt
-    Minimum distance: d_min ≥ 2t + 1.
-    '''
-    res = P_cw(12, 3, 0.001)
-    print(res)
-    res = P_cw(12, 3, 0.002)
-    print(res)
-    res = P_cw(4, 3, 0.002)
-    print(res)
-
 
 def load_db():
     global db
     with open('database.json', 'rb') as fin:
         db = pickle.load(fin)
 
-def select_bound():
+
+def get_all_candidates(prune=False):
     res = []
     for key in db.keys():
         q, n, k = key
         d = db[key]
         assert d <= n - k + 1
-        if d == n - k + 1:
+        if prune:
+            if d == n - k + 1:
+                res.append((q, n, k, d))
+        else:
             res.append((q, n, k, d))
     return res
 
@@ -52,29 +43,42 @@ def select_ratio(candidates, ratio):
     res = []
     for cand in candidates:
         q, n, k, d = cand
-        uber = P_cw(n, int((d-1)/2), 0.0001)
-        if uber < ratio:
-            res.append(cand)
+        p_cw = P_cw(n, int((d-1)/2), 0.01)
+        # User_Data_bits_per_CW = log(pow(q^m, k), base=2) = k / log(2, base=q^m)
+        bits_per_cw = k / math.log(2, q)
+        uber = p_cw / bits_per_cw
+        if uber <= ratio:
+            res.append((cand, uber))
     return res
 
 def best_overhead(candidates):
     minimum = 10
     best_cand = []
-    for cand in candidates:
+    for cand_uber in candidates:
+        cand, uber = cand_uber
         q, n, k, d = cand
-        overhead = 1 - k / n
-        if overhead <= minimum:
+        overhead = n / k
+        if overhead < minimum:
             minimum = overhead
-            best_cand.append((q, n, k, d))
-    return best_cand, overhead
+            best_cand = [(q, n, k, d, uber)]
+        elif overhead == minimum:
+            best_cand.append((q, n, k, d, uber))
+    return best_cand, minimum
 
 
 if __name__ == "__main__":
     load_db()
-    res = select_bound()
-    # print(len(res))
+    res = get_all_candidates(prune=False)
     res = select_ratio(res, 1e-16)
-    print(res[:5])
-    # cand, overhead = best_overhead(res)
-    # print(len(cand), overhead)
-    # print(cand)
+    cand, overhead = best_overhead(res)
+    print(len(cand), overhead)
+    print("q, n, k, d, uber")
+    print(cand)
+    # RBER: 0.01
+    # With Pruning:
+    # No
+
+    # Without Pruning:
+    # 1 1.6538461538461537
+    # q, n, k, d, uber
+    # [(16, 129, 78, 33, 7.857483952855776e-17)]
