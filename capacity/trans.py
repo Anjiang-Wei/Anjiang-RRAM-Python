7e4b9577edb819b4a5fcba5979044672c16e500a
Anjiang-Wei <1020681930@qq.com>
2022-02-01 14:50:30 -0800

    modify trans

--- capacity/trans.py
+++ capacity/trans.py
@@ -19,17 +19,34 @@ def write_matrix(matrix, filename):
             to_write.append(",".join(map(str, matrix[i])) + "\n")
         fout.writelines(to_write)
 
-def trans(int1, prefix):
+def trans(int1, merge, prefix):
     m1 = get_matrix_from_file(prefix + str(int1))
     print(m1)
-    int2 = int1 // 2
+    int2 = int1 - 1
     m2 = np.zeros((int2, int2))
     for i in range(int2):
         for j in range(int2):
-            m2[i][j] = (m1[i*2][j*2] + m1[i*2][j*2+1] + m1[i*2+1][j*2] + m1[i*2+1][j*2+1]) / 2
+            if i == merge or j == merge:
+                continue
+            idxi = i
+            idxj = j
+            if i > merge:
+                idxi += 1
+            if j > merge:
+                idxj += 1
+            m2[i][j] = m1[i if i < merge else (i + 1)][j if j < merge else (j + 1)]
+    for j in range(int1):
+        m1[merge][j] += m1[merge+1][j]
+    for i in range(int1):
+        m1[i][merge] += m1[i][merge+1]
+    for j in range(int2):
+        m2[merge][j] = m1[merge][j if j <= merge else (j + 1)]
+    for i in range(int2):
+        m2[i][merge] = m1[i if i <= merge else (i + 1)][merge] / 2
     print(m2)
     write_matrix(m2, prefix + str(int2))
 
 if __name__ == "__main__":
-    trans(10, prefix="ours")
-    trans(8, prefix="ours")
+    trans(6, 2, prefix="ours")
+    trans(5, 1, prefix="ours")
+
