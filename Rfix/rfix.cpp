e7a846db2274095af5420500bceb0e99ba0463bb
Anjiang-Wei <1020681930@qq.com>
2022-02-22 18:41:08 +0000

    now I am confused, who can save me

--- Rfix/rfix.cpp
+++ Rfix/rfix.cpp
@@ -22,39 +22,40 @@ class Rfix {
 public:
   uint8_t R; // radix
   uint8_t M; // number of bits in content
-  bool sign; // whether this is signed Rfix or unsigned (positive) Rfix
+  bool sign; // whether this is signed Rfix (can be negative) or unsigned (always positive) Rfix
   uint8_t content[maxE]; // each element should be within [0, radix-1]
 
   Rfix();
   Rfix(uint8_t R_, uint8_t M_, bool sign_, uint8_t content_[maxM]);
-  Rfix(uint8_t R_, uint8_t M_, bool sign_, int val);
+  Rfix(uint8_t R_, uint8_t M_, bool sign_, long long val);
 
   friend ofstream& operator<<(ofstream &output, const Rfix &D);
   friend ifstream& operator>>(ifstream &input, const Rfix &D);
 
   void print();
-  int from_Rfix();
+  long long from_Rfix();
   void mutate(int m_p, int m_a, float spec_ber, float raw_ber);
   static vector<Rfix> mutate_vec_Rfix(vector<Rfix> input,
                                       int m_p, int m_a, float spec_ber, float raw_ber);
-  static vector<float> mutate_vec_int(vector<int> input, int R, int M, bool sign,
+  static vector<float> mutate_vec_int(vector<long long> input, int R, int M, bool sign,
                                       int m_p, int m_a, float spec_ber, float raw_ber);
 };
 
-Rfloat::Rfloat() {
+Rfix::Rfix() {
   return;
 }
 
-Rfloat::Rfloat(uint8_t R_, uint8_t M_, bool sign_, uint8_t content_[maxM]) {
+Rfix::Rfix(uint8_t R_, uint8_t M_, bool sign_, uint8_t content_[maxM]) {
   R = R_; M = M_; sign = sign_;
   for (int i = 0; i < M; i++) {
     content[i] = content_[i];
   }
 }
 
-Rfloat::Rfloat(uint8_t R_, uint8_t M_, bool sign_, int val) {
+Rfix::Rfix(uint8_t R_, uint8_t M_, bool sign_, long long val) {
     R = R_; M = M_; sign = sign_;
     //Todo: implement this
+
 }
 
 ofstream& operator<<(ofstream& output, const Rfloat& D) {
@@ -67,40 +68,46 @@ ofstream& operator<<(ofstream& output, const Rfloat& D) {
 }
 
 inline uint8_t get_uint8(ifstream &input) {
-    int x;
-    input >> x;
-    return (uint8_t) x;
+  int x;
+  input >> x;
+  return (uint8_t) x;
 }
 
 ifstream& operator>>(ifstream &input, Rfloat &D){
-    D.R = get_uint8(input);
-    D.M = get_uint8(input);
-    D.sign = (bool) get_uint8(input);
-    for (int i = 0; i < D.M; i++) {
-        D.content[i] = get_uint8(input);
-    }
-    return input;
+  D.R = get_uint8(input);
+  D.M = get_uint8(input);
+  D.sign = (bool) get_uint8(input);
+  for (int i = 0; i < D.M; i++) {
+    D.content[i] = get_uint8(input);
+  }
+  return input;
 }
 
-void Rfloat::print() {
-    cout << "Radix: " << (int) R << endl;
-    cout << "M: " << (int) M << ": " << endl;
-    cout << "Sign : " << sign << endl;
-    for (int i = 0; i < M; i++) {
-        cout << (int) content[i];
-    }
-    cout << endl;
+void Rfix::print() {
+  cout << "Radix: " << (int) R << endl;
+  cout << "M: " << (int) M << ": " << endl;
+  cout << "Sign : " << sign << endl;
+  for (int i = 0; i < M; i++) {
+    cout << (int) content[i];
+  }
+  cout << endl;
 }
 
 
-int Rfloat::from_Rfloat() {
-  // Todo: implement this
+long long Rfix::from_Rfix() {
+  long long val = 0;
+  long long base = 1;
+  for (int i = 0; i < M; i++) {
+    val += base * content[i];
+    base = base * R;
+  }
+  
   return val;
 }
 
 inline bool random_bool(float ber) {
-    // srand(time(0));
-    float rand_gen = dis(gen);
+  // srand(time(0));
+  float rand_gen = dis(gen);
     bool TrueFalse = rand_gen < (ber * 1e15);
     return TrueFalse;
 }
@@ -127,7 +134,7 @@ void print_uint8(uint8_t array[], int num) {
     cout << endl << "-------" << endl;
 }
 
-void Rfloat::mutate(int m_p, int m_a, float spec_ber, float raw_ber) {
+void Rfix::mutate(int m_p, int m_a, float spec_ber, float raw_ber) {
     #ifdef DEBUG
     cout << "Prior" << endl;
     print_uint8(exp, E);
@@ -148,32 +155,33 @@ void Rfloat::mutate(int m_p, int m_a, float spec_ber, float raw_ber) {
     print_uint8(exp, E);
     print_uint8(mant, M);
     cout << "==============" << endl;
-    #endif
+#endif
 }
 
 static vector<Rfix> mutate_vec_Rfix(vector<Rfix> input,
-    int m_p, int m_a, float spec_ber, float raw_ber) {
-    int size = input.size();
-    for (int i = 0; i < size; i++) {
-        input[i].mutate(m_p, m_a, spec_ber, raw_ber);
-    }
-    return input;
+                                    int m_p, int m_a, float spec_ber, float raw_ber) {
+  int size = input.size();
+  for (int i = 0; i < size; i++) {
+    input[i].mutate(m_p, m_a, spec_ber, raw_ber);
+  }
+  return input;
 }
 
 
-static vector<int> mutate_vec_int(vector<int> input, int R, int M, bool sign,
-                                  int m_p, int m_a, float spec_ber, float raw_ber);
-    int size = input.size();
-    uint8_t R_ = (uint8_t) R;
-    uint8_t M_ = (uint8_t) M;
-    for (int i = 0; i < size; i++) {
-        int val = input[i];
-        Rfix a = Rfloat(R_, M_, val);
-        a.mutate(m_p, m_a, spec_ber, raw_ber);
-        input[i] = a.from_Rfloat();
-        #ifdef DEBUG
-            cout << val << " " << input[i] << endl;
-        #endif
-    }
-    return input;
+static vector<int> mutate_vec_int(vector<long long> input, int R, int M, bool sign,
+                                  int m_p, int m_a, float spec_ber, float raw_ber)
+{
+  int size = input.size();
+  uint8_t R_ = (uint8_t) R;
+  uint8_t M_ = (uint8_t) M;
+  for (int i = 0; i < size; i++) {
+    int val = input[i];
+    Rfix a = Rfloat(R_, M_, val);
+    a.mutate(m_p, m_a, spec_ber, raw_ber);
+    input[i] = a.from_Rfloat();
+#ifdef DEBUG
+    cout << val << " " << input[i] << endl;
+#endif
+  }
+  return input;
 }
