afbc7f89003a6e1e566f6512ae1360962bf01590
Anjiang-Wei <1020681930@qq.com>
2022-03-01 23:06:39 +0000

    remove scale from C++

--- sobel/Fixed_mutate.cpp
+++ sobel/Fixed_mutate.cpp
@@ -7,7 +7,7 @@ using namespace std;
 #define VERB
 
 int main(int argc, char* argv[]) {
-    assert(argc == 10);
+    assert(argc == 9);
     ifstream infile;
     infile.open(argv[1]);
     ofstream outfile;
@@ -15,7 +15,6 @@ int main(int argc, char* argv[]) {
 
     int R; int M;
     int m_p; int m_a; // m_p + m_a = M
-    float scale; // RRAM is actually storing (input_num * scale)
     float spec_ber; float raw_ber;
 
     R = atoi(argv[3]);
@@ -24,29 +23,28 @@ int main(int argc, char* argv[]) {
     m_a = atoi(argv[6]);
     assert(m_p + m_a == M);
 
-    scale = atof(argv[7]);
-    spec_ber = atof(argv[8]);
-    raw_ber = atof(argv[9]);
+    spec_ber = atof(argv[7]);
+    raw_ber = atof(argv[8]);
 
     vector<long long> all_fixed;
 
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
 
-    float x;
+    long long x;
     while (infile >> x) {
-      all_fixed.push_back((long long)(x * scale));
+      all_fixed.push_back(x);
     }
     #ifdef VERB
-    cout << "Fixed_mutate configutation " << argv[1] << " " << argv[2] << " " 
+    cout << "Fixed_mutate configutation " << argv[1] << " " << argv[2] << " "
         << R << " " << M
         << " " << m_p  << " " <<  m_a
-        << " " << scale << " " << spec_ber  << " " << raw_ber << std::endl;
+        << " " << spec_ber  << " " << raw_ber << std::endl;
     #endif
     auto output = mutate_vec_ll(all_fixed, R, M, m_p, m_a, spec_ber, raw_ber);
 
     for (auto item: output) {
-      outfile << (long long)(((float)item) / scale) << endl;
+      outfile << item << endl;
     }
     infile.close();
     outfile.close();
