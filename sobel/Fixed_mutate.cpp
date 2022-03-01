629f19c754ffbade6921ea396212d317cc9376c4
Anjiang-Wei <1020681930@qq.com>
2022-03-01 22:07:56 +0000

    driver for Fixed mutate

---
+++ sobel/Fixed_mutate.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+# include "../Rfix/rfix.cpp"
+# include <stdlib.h>
+#include <assert.h>
+using namespace std;
+
+#define VERB
+
+int main(int argc, char* argv[]) {
+    assert(argc == 10);
+    ifstream infile;
+    infile.open(argv[1]);
+    ofstream outfile;
+    outfile.open(argv[2]);
+
+    int R; int M;
+    int m_p; int m_a; // m_p + m_a = M
+    float scale; // RRAM is actually storing (input_num * scale)
+    float spec_ber; float raw_ber;
+
+    R = atoi(argv[3]);
+    M = atoi(argv[4]);
+    m_p = atoi(argv[5]);
+    m_a = atoi(argv[6]);
+    assert(m_p + m_a == M);
+
+    scale = atof(argv[7]);
+    spec_ber = atof(argv[8]);
+    raw_ber = atof(argv[9]);
+
+    vector<long long> all_fixed;
+
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    float x;
+    while (infile >> x) {
+      all_fixed.push_back((long long)(x * scale));
+    }
+    #ifdef DEBUG
+    cout << "Fixed_mutate configutation " << argv[1] << " " << argv[2] << " " 
+        << R << " " << M
+        << " " << m_p  << " " <<  m_a
+        << " " << scale << " " << spec_ber  << " " << raw_ber << std::endl;
+    #endif
+    auto output = mutate_vec_ll(all_fixed, R, M, m_p, m_a, spec_ber, raw_ber);
+
+    for (auto item: output) {
+      outfile << (long long)(((float)item) / scale) << endl;
+    }
+    infile.close();
+    outfile.close();
+    #ifdef DEBUG
+    cout << "finished!!" << endl;
+    #endif
+    return 0;
+}
