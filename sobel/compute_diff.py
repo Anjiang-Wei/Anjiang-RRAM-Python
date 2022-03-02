e53022e25783383c3d90913eeeb515bdf3b546a9
Anjiang-Wei <1020681930@qq.com>
2022-03-02 07:18:33 +0000

    add file comparison in compute_diff

--- sobel/compute_diff.py
+++ sobel/compute_diff.py
@@ -24,6 +24,26 @@ def diff(f1, f2):
     assert len(res1) == len(res2)
     return root_mean_square(res1, res2)
 
+def all_files(suffix):
+    res1 = []
+    res2 = []
+    for i in range(1, 38):
+        if i == 29:
+            continue
+        res1.append("intermediate/" + str(i) + "." + suffix)
+        res2.append("mutated/" + str(i) + "." + suffix)
+    assert len(res1) == 36 and len(res2) == 36
+    return res1, res2
+
+def diffall(only3):
+    f1s, f2s = all_files("out")
+    if only3:
+        f1s, f2s = f1s[:3], f2s[:3]
+    all_diff = 0
+    for i in range(0, len(f1s)):
+        ediff = diff(f1s[i], f2s[i])
+        all_dff += ediff
+    return all_diff / len(f1s)
 
 if __name__ == "__main__":
     f1 = sys.argv[1]
