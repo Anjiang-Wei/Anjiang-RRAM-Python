8c3f87f8f027984c06e2644729a05cad560105e2
Anjiang-Wei <1020681930@qq.com>
2022-03-18 20:23:18 +0000

    assertion fails

--- Rfloat/test_rfloat.cpp
+++ Rfloat/test_rfloat.cpp
@@ -61,7 +61,7 @@ int main() {
     // -354: 1 10000111 01100010000000000000000
     uint8_t exp[8] = {1, 0, 0, 0, 0, 1, 1, 1};
     uint8_t mant[23] = {0,1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    Rfloat a = Rfloat(2, 8, 23, true, 1, exp, mant);
+    Rfloat a = Rfloat(2, 8, 23, true, exp, mant);
     float b = a.from_Rfloat();
     a.print();
     cout << b << endl;
