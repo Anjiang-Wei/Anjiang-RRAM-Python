5f171306238aa34e53bf4038c84505d39a3063fe
Anjiang-Wei <1020681930@qq.com>
2022-03-18 20:19:52 +0000

    hopefully without leadingM everything can work

--- Rfloat/rfloat.cpp
+++ Rfloat/rfloat.cpp
@@ -14,7 +14,7 @@ using namespace std;
 #define maxM 40
 
 // #define DEBUG
-#define inf2zero
+// #define inf2zero
 
 random_device rd;  // Will be used to obtain a seed for the random number engine
 std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
@@ -24,15 +24,14 @@ class Rfloat {
     public: 
         uint8_t R; // radix
         uint8_t E; // number of bits in exponent
-        uint8_t M; // number of bits in mantissa. The actual bits for mantissa is: (leadingM) 1 + M
+        uint8_t M; // number of bits in mantissa.
         bool sign;
         int bias; // to be deducted when computing exponent
-        uint8_t leadingM; // within [1, R-1]
         uint8_t exp[maxE]; // each element should be within [0, radix-1]
         uint8_t mant[maxM];
 
         Rfloat();
-        Rfloat(uint8_t R_, uint8_t E_, uint8_t M_, bool sign_, uint8_t leadingM_, uint8_t exp_[maxE], uint8_t mant_[maxM]);
+        Rfloat(uint8_t R_, uint8_t E_, uint8_t M_, bool sign_, uint8_t exp_[maxE], uint8_t mant_[maxM]);
         Rfloat(uint8_t R_, uint8_t E_, uint8_t M_, float val);
 
         friend ofstream& operator<<(ofstream &output, const Rfloat &D);
@@ -51,8 +50,8 @@ Rfloat::Rfloat() {
     return;
 }
 
-Rfloat::Rfloat(uint8_t R_, uint8_t E_, uint8_t M_, bool sign_, uint8_t leadingM_, uint8_t exp_[maxE], uint8_t mant_[maxM]) {
-    R = R_; E = E_; M = M_; sign = sign_; leadingM = leadingM_;
+Rfloat::Rfloat(uint8_t R_, uint8_t E_, uint8_t M_, bool sign_, uint8_t exp_[maxE], uint8_t mant_[maxM]) {
+    R = R_; E = E_; M = M_; sign = sign_;
     for (int i = 0; i < maxE; i++) {
         exp[i] = exp_[i];
     }
@@ -73,7 +72,6 @@ Rfloat::Rfloat(uint8_t R_, uint8_t E_, uint8_t M_, float val) {
     }
     if (val == 0) {
         sign = false;
-        leadingM = 0;
         for (int i = 0; i < maxE; i++) {
             exp[i] = 0;
         }
@@ -90,7 +88,7 @@ Rfloat::Rfloat(uint8_t R_, uint8_t E_, uint8_t M_, float val) {
     val = fabs(val);
     int y = (int) (log(val) / log(R));
     int exp_val = y + bias;
-    if (exp_val < 0) {
+    if (exp_val < 0) { // if original exp_val negative, Rfloat has to turn its exp_val into 0
         y = y - exp_val;
         exp_val = 0;
     }
@@ -103,13 +101,13 @@ Rfloat::Rfloat(uint8_t R_, uint8_t E_, uint8_t M_, float val) {
     }
     float R_to_y = pow(R, y);
     float remainder = val / R_to_y;
-    leadingM = (int) remainder;
-    assert(leadingM >= 0 && leadingM < R);
-    remainder -= leadingM;
+    mant[0] = (uint8_t)((int) remainder);
+    assert(mant[0] >= 0 && mant[0] < R);
+    remainder -= ((int) remainder);
     #ifdef DEBUG
         cout << "remainder = " << remainder << endl;
     #endif
-    for (int i = 0; i < M; i++) {
+    for (int i = 1; i < M; i++) {
         remainder *= R;
         mant[i] = (int) remainder;
         remainder -= (int) remainder;
@@ -118,7 +116,7 @@ Rfloat::Rfloat(uint8_t R_, uint8_t E_, uint8_t M_, float val) {
 
 ofstream& operator<<(ofstream& output, const Rfloat& D) {
     output << (int) D.R << " " << (int) D.E << " " << (int) D.M <<  " " << (int) D.sign 
-        << " " << (int) D.bias << " " << (int) D.leadingM << endl;
+        << " " << (int) D.bias << endl;
     for (int i = 0; i < D.E; i++) {
         output << (int) D.exp[i] << " ";
     }
@@ -141,8 +139,10 @@ ifstream& operator>>(ifstream &input, Rfloat &D){
     D.E = get_uint8(input);
     D.M = get_uint8(input);
     D.sign = (bool) get_uint8(input);
-    input >> D.bias;
-    D.leadingM = get_uint8(input);
+    D.bias = (int) pow(D.R, D.E-1) - 1;
+    if (D.E == 0) {
+        D.bias = 0;
+    }
     for (int i = 0; i < D.E; i++) {
         D.exp[i] = get_uint8(input);
     }
@@ -160,7 +160,6 @@ void Rfloat::print() {
         cout << (int) exp[i];
     }
     cout << endl << "Man-" << (int) M << ": " << endl;
-    cout << (int) leadingM << ".";
     for (int i = 0; i < M; i++) {
         cout << (int) mant[i];
     }
@@ -169,9 +168,12 @@ void Rfloat::print() {
 
 
 float Rfloat::from_Rfloat() {
-    float val = (float) leadingM;
+    if (M == 0) {
+        return 0;
+    }
+    float val = mant[0];
     float weight = R;
-    for (int i = 0; i < M; i++) {
+    for (int i = 1; i < M; i++) {
         val += ((float) mant[i]) / weight;
         weight = weight * R;
         // cout << "weight:" << weight << endl;
@@ -216,7 +218,7 @@ inline uint8_t mutate_uint8(uint8_t original, uint8_t R) {
     }
     if (random_bool(0.5)) {
         return original + 1;
-    } else{ 
+    } else {
         return original - 1;
     }
 }
@@ -236,24 +238,22 @@ void Rfloat::mutate(int m_p, int m_a, float spec_ber, float raw_ber) {
     print_uint8(mant, M);
     #endif
     for (int i = 0; i < E; i++) {
+        // E: always precise
         if (random_bool(spec_ber)) {
             exp[i] = mutate_uint8(exp[i], R);
         }
     }
     for (int i = 0; i < m_p; i++) {
+        // m_p: precise
         if (random_bool(spec_ber)) {
-            if (i == 0) {
-                leadingM = mutate_uint8(leadingM, R);
-            } else {
-                mant[i-1] = mutate_uint8(mant[i-1], R);
-            }
+            mant[i] = mutate_uint8(mant[i], R);
         }
     }
-    for (int i = 0; i < m_a; i++) {
+    assert(m_a + m_p == M);
+    for (int i = m_p; i < m_p + m_a; i++) {
+        // m_a: approximate
         if (random_bool(raw_ber)) {
-            int start_idx = ((m_p - 1) > 0 ? (m_p - 1) : 0);
-            int cur_idx = start_idx + i;
-            mant[cur_idx] = mutate_uint8(mant[cur_idx], R);
+            mant[i] = mutate_uint8(mant[i], R);
         }
     }
     #ifdef DEBUG
@@ -264,7 +264,7 @@ void Rfloat::mutate(int m_p, int m_a, float spec_ber, float raw_ber) {
     #endif
 }
 
-static vector<Rfloat> mutate_vec_Rfloat(vector<Rfloat> input, 
+static vector<Rfloat> mutate_vec_Rfloat(vector<Rfloat> input,
     int m_p, int m_a, float spec_ber, float raw_ber) {
     int size = input.size();
     for (int i = 0; i < size; i++) {
@@ -277,15 +277,15 @@ static vector<Rfloat> mutate_vec_Rfloat(vector<Rfloat> input,
 static vector<float> mutate_vec_float(vector<float> input,
     int R, int E, int M, int m_p, int m_a, float spec_ber, float raw_ber) {
     /*
-    R, E, M: base, number of bits for exponent, mantissa [including leading] 
+    R, E, M: base, number of bits for exponent, mantissa
     m_p, m_a: m_p + m_a = M; number of precise/approximate bits
     spec_ber: specification for precise drift error probability
-    raw_ber: for the base R, the drift error probability 
+    raw_ber: for the base R, the drift error probability
     */
     int size = input.size();
     uint8_t R_ = (uint8_t) R;
     uint8_t E_ = (uint8_t) E;
-    uint8_t M_ = (uint8_t) (M - 1);
+    uint8_t M_ = (uint8_t) M;
     for (int i = 0; i < size; i++) {
         float val = input[i];
         Rfloat a = Rfloat(R_, E_, M_, val);
