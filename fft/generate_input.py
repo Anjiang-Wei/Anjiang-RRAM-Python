1af49e3d645118f5b28233509a927008f1001654
Anjiang-Wei <1020681930@qq.com>
2022-03-25 21:11:29 +0000

    2^12 input seems better

--- fft/generate_input.py
+++ fft/generate_input.py
@@ -1,5 +1,9 @@
+import sys
+
+num = int(sys.argv[1])
+
 lines = []
-for i in range(0, 2048):
+for i in range(0, 4096):
     lines.append(str(i)+"\n")
 with open("input0", "w") as fout:
     fout.writelines(lines)
