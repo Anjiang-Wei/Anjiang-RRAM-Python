e3afaa6b9d471645c55aecf0a6175dc16383f563
Anjiang-Wei <1020681930@qq.com>
2022-08-17 16:32:14 -0700

    restart C13 exp

--- test_scheme.py
+++ test_scheme.py
@@ -27,7 +27,7 @@ test_scheme_files = [
     # 'scheme/SBA/C13_both_15.json',
     # 'scheme/SBA/C13_both_16.json',
 ]
-random_seed = 29 + exp_id
+random_seed = 31 + exp_id
 levels = Level.load_from_file(test_scheme_files[exp_id-6])
 high_init_config = {
     "B": [levels[-1].r1, levels[-1].r2]
@@ -106,7 +106,7 @@ if __name__ == "__main__":
     print("Num of dead cells", len(dead_cells))
     nisys = NIRRAM(chipname)
     n_cells = 100
-    log = open(f"testlog/13scheme_test_{n_cells}_{random_seed}_{exp_id}_July15", "w")
+    log = open(f"testlog/13scheme_test_{n_cells}_{random_seed}_{exp_id}_Aug17", "w")
     testscheme(n_cells)
     nisys.close()
     log.close()
