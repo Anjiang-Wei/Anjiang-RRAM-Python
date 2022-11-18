a384f476f424a172288a7a2c31611f33e832132a
Anjiang-Wei <1020681930@qq.com>
2022-11-17 22:53:28 -0800

    upload this wrong file

--- capacity/trans.py
+++ capacity/trans.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pprint
 
 def get_matrix_from_file(filename):
     with open(filename, "r") as fin:
@@ -12,41 +13,34 @@ def get_matrix_from_file(filename):
                 matrix[i][j] = line[j]
     return matrix
 
-def write_matrix(matrix, filename):
-    with open(filename, "w") as fout:
-        to_write = []
-        for i in range(len(matrix)):
-            to_write.append(",".join(map(str, matrix[i])) + "\n")
-        fout.writelines(to_write)
+# def write_matrix(matrix, filename):
+#     with open(filename, "w") as fout:
+#         to_write = []
+#         for i in range(len(matrix)):
+#             to_write.append(",".join(map(str, matrix[i])) + "\n")
+#         fout.writelines(to_write)
 
-def trans(int1, merge, prefix):
-    m1 = get_matrix_from_file(prefix + str(int1))
-    print(m1)
-    int2 = int1 - 1
-    m2 = np.zeros((int2, int2))
-    for i in range(int2):
-        for j in range(int2):
-            if i == merge or j == merge:
-                continue
-            idxi = i
-            idxj = j
-            if i > merge:
-                idxi += 1
-            if j > merge:
-                idxj += 1
-            m2[i][j] = m1[i if i < merge else (i + 1)][j if j < merge else (j + 1)]
-    for j in range(int1):
-        m1[merge][j] += m1[merge+1][j]
-    for i in range(int1):
-        m1[i][merge] += m1[i][merge+1]
-    for j in range(int2):
-        m2[merge][j] = m1[merge][j if j <= merge else (j + 1)]
-    for i in range(int2):
-        m2[i][merge] = m1[i if i <= merge else (i + 1)][merge] / 2
-    print(m2)
-    write_matrix(m2, prefix + str(int2))
+def compute_average(matrix):
+    error_rate = 0
+    for i in range(len(matrix)):
+        error_rate += 1 - matrix[i][i]
+    error_rate = error_rate / len(matrix)
+    return error_rate
+            
+def report_results(filename_prefix):
+    res = {}
+    for i in range(4, 9):
+        fname = filename_prefix + str(i)
+        matrix = get_matrix_from_file(fname)
+        avg = compute_average(matrix)
+        res[i] = avg
+    print(filename_prefix + "=")
+    pprint.pprint(res)
 
 if __name__ == "__main__":
-    trans(6, 2, prefix="ours")
-    trans(5, 1, prefix="ours")
-
+    report_results("ours")
+    report_results("SBA")
+    report_results("SBAvar")
+    report_results("SBAmeanvar")
+# we should use this file for final results reported in the paper:
+# scheme_analyze.py
