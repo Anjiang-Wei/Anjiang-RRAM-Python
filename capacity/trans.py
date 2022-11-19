26348edf80308f42df7c2fcc92653f25da9b4899
Anjiang-Wei <1020681930@qq.com>
2022-11-19 13:38:56 -0800

    add ber script

--- capacity/trans.py
+++ capacity/trans.py
@@ -37,6 +37,53 @@ def report_results(filename_prefix, hint):
     print(hint + " = \\")
     pprint.pprint(res)
 
+gray_coding = \
+{
+    4: ["00", "01", "11", "10"],
+    8: ["000", "001", "011", "010", "110", "111", "101", "100"]
+}
+dist_4 = np.zeros((4, 4))
+dist_8 = np.zeros((8, 8))
+
+def str_diff(s1, s2):
+    assert len(s1) == len(s2)
+    diff = 0
+    for i in range(0, len(s1)):
+        if s1[i] != s2[i]:
+            diff += 1
+    assert diff <= len(s1)
+    return diff
+
+def init_dist():
+    for i in range(0, 4):
+        for j in range(0, 4):
+            dist_4[i][j] = str_diff(gray_coding[4][i], gray_coding[4][j])
+    for i in range(0, 8):
+        for j in range(0, 8):
+            dist_8[i][j] = str_diff(gray_coding[8][i], gray_coding[8][j])
+    # pprint.pprint(dist_4)
+    # pprint.pprint(dist_8)
+
+def report_ber(filename_prefix, level_list):
+    for i in level_list:
+        dist = 0
+        num_bits = 0
+        if i == 4:
+            dist = dist_4
+            num_bits = 2
+        elif i == 8:
+            dist = dist_8
+            num_bits = 3
+        else:
+            assert False
+        fname = filename_prefix + str(i)
+        matrix = get_matrix_from_file(fname)
+        ber_matrix = np.multiply(matrix, dist) / i
+        # pprint.pprint(ber_matrix)
+        ber_avg = np.sum(ber_matrix) / num_bits
+        print(filename_prefix + str(i) + " =", ber_avg)
+
+
 if __name__ == "__main__":
     report_results("ours", "our_res")
     report_results("SBA", "sba_res")
@@ -44,3 +91,6 @@ if __name__ == "__main__":
     report_results("SBAmeanvar", "sba_our_search_mean")
 # we should use this file for final results reported in the paper
 # instead of scheme_analyze.py (which is non-uniform weighted average)
+    init_dist()
+    report_ber("ours", [4, 8])
+    report_ber("SBA", [4, 8])
